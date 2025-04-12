4329f367fb3532504d620d3161a7654680771ba3
Ankit Budhiraja <155446368+Ankro123@users.noreply.github.com>
2025-04-12 11:39:57 +0530

    Update DatabaseInitializer.java

--- src/managers/DatabaseInitializer.java
+++ src/managers/DatabaseInitializer.java
@@ -1,5 +1,8 @@
 package src.managers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -9,6 +12,7 @@ public class DatabaseInitializer {
     private static final String url = "jdbc:mysql://localhost:3306";
     private static String user = "";
     private static String password = "";
+
     public static void initialize() {
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
@@ -20,37 +24,37 @@ public class DatabaseInitializer {
             stmt = conn.createStatement();
 
             stmt.executeUpdate(
-                "CREATE TABLE IF NOT EXISTS Student (" +
-                "ERP_ID VARCHAR(10) PRIMARY KEY, " +
-                "Name VARCHAR(100), " +
-                "Course VARCHAR(100))");
+                    "CREATE TABLE IF NOT EXISTS Student (" +
+                            "ERP_ID VARCHAR(10) PRIMARY KEY, " +
+                            "Name VARCHAR(100), " +
+                            "Course VARCHAR(100))");
 
             stmt.executeUpdate(
-                "CREATE TABLE IF NOT EXISTS Book (" +
-                "Book_ID VARCHAR(10) PRIMARY KEY, " +
-                "Title VARCHAR(100), " +
-                "Author VARCHAR(100), " +
-                "Availability ENUM('Yes', 'No') DEFAULT 'Yes')");
+                    "CREATE TABLE IF NOT EXISTS Book (" +
+                            "Book_ID VARCHAR(10) PRIMARY KEY, " +
+                            "Title VARCHAR(100), " +
+                            "Author VARCHAR(100), " +
+                            "Availability ENUM('Yes', 'No') DEFAULT 'Yes')");
 
             stmt.executeUpdate(
-                "CREATE TABLE IF NOT EXISTS Transactions (" +
-                "Transaction_ID INT AUTO_INCREMENT PRIMARY KEY, " +
-                "Student_ERP VARCHAR(10), " +
-                "Book_ID VARCHAR(10), " +
-                "Type ENUM('Borrow', 'Return'), " +
-                "Transaction_Date DATE, " +
-                "Due_Date DATE)");
+                    "CREATE TABLE IF NOT EXISTS Transactions (" +
+                            "Transaction_ID INT AUTO_INCREMENT PRIMARY KEY, " +
+                            "Student_ERP VARCHAR(10), " +
+                            "Book_ID VARCHAR(10), " +
+                            "Type ENUM('Borrow', 'Return'), " +
+                            "Transaction_Date DATE, " +
+                            "Due_Date DATE)");
 
             stmt.executeUpdate(
-                "CREATE TABLE IF NOT EXISTS Users (" +
-                "Username VARCHAR(20) PRIMARY KEY, " +
-                "Password VARCHAR(20));"
-            );
+                    "CREATE TABLE IF NOT EXISTS Users (" +
+                            "Username VARCHAR(20) PRIMARY KEY, " +
+                            "Password VARCHAR(20));");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     public static void getCreds() throws IOException {
         String creds = Files.readString(Path.of("lib/SQLConfig.txt"));
         String[] arr = creds.split("\n");
